60fafcaaa4ffbe25ddfc64c5c73ef3a85478efd6
Lincoln123 <zek_95_95@mail.ru>
2015-11-04 13:35:30 +0300

    2

--- dz/dz/dz.py
+++ dz/dz/dz.py
@@ -1,4 +1,4 @@
-#f = open('data.txt','r')
+﻿#f = open('data.txt','r')
 import math
 n = int(input("Enter the figure "))
 if (n<=1): 
@@ -10,6 +10,18 @@ else:
         if m%1==0:
             print('False, Composite number')
             break
+    else: #я думаю надо эту часть сдвинуть но куда пока не понял
+        j=int(1)
+        q=int(1)
+        while j >= 1:
+               while int(q)>=j+1:
+                    (n**j)%q!=1
+                    r=q
+                    print (r)
+                    break
+
+            
+
         #break
     #print('False, Composite number')
 
