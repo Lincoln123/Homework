c7ce4abfd68c54ff2c3d6f39459c37357660ef8f
Lincoln123 <zek_95_95@mail.ru>
2015-12-14 01:43:00 +0300

    xz

--- dz/dz/dz.py
+++ dz/dz/dz.py
@@ -1,8 +1,21 @@
 ﻿import math
 import sys
-
 fil = sys.argv[1]
-fil1 = sys.argv[2]
+fil1 = sys.argv[2] 
+
+def Main():  
+    f = open(fil,'r')
+    n = int(f.read()) #переменная входного числа
+    q = int(4 * pow(math.log2(n),2) + 1)    
+    check_input(n)
+    PerfectPower(n)
+    Individual_r(n,q)
+
+def check_input(n):
+    if n < 1 : 
+        Output('\nFalse, input is incorrect')
+    elif n == 1 :
+        Output('\nFalse, Composite number')
 
 def Output(text) :
     f = open(fil1, 'w')
@@ -10,11 +23,11 @@ def Output(text) :
     f.close()
     exit(0)
      
-def PerfectPower():
+def PerfectPower(n):
     b = 2                        #показатель степени
     if b > math.log2(n):         #проверка на вход 
         Output('prime')
-        #exit(0)   
+        
     while b <= math.log2(n): 
         m = math.ceil(n**(1/b))  #корень b'ой степени из n
         if n == m**b:
@@ -23,34 +36,34 @@ def PerfectPower():
         else:
             b+=1
             
-          
-def Individual_r():
-    q = int(4 * pow(math.log2(n),2) + 1) #делитель для определения числа r
-    while q > 4 * pow(math.log2(n),2) :
-        for j in range(1,int(4 * pow(math.log2(n),2))) :   #степень числа n
-            if (n ** j) % q != 1 :
-                r = q
-            else: 
-                q += 1
-            if (r) :
-                return [GCD(r),r]
+def Individual_r(n,q):
+    for j in range(1,int(4 * pow(math.log2(n),2))) :   #степень числа n
+        if (n ** j) % q != 1 :
+            r = q
+            GCD(n,r)
+            FermaTest(n,r)
+            continue
+        else: 
+            q += 1
+            return Individual_r(q)#,[GCD(r),r]
                 
-def GCD(r):    
+def GCD(n,r):    
     if r >= n:
         r = n - 1
-        for a in range(r,0,-1) :
+        for gcd in range(r,0,-1) :
             z = n
-            if z % a == 0 :
-                return a        
+            if z % gcd == 0 :
+                if gcd != 1:
+                    Output('\nFalse, Composite number')        
     else:
-        for a in range(r,0,-1) :
+        for gcd in range(r,0,-1) :
             z = n
+            if z % gcd == 0 :
+                if gcd != 1:
+                    Output('\nFalse, Composite number')
                     
-            if z %a == 0 :
-                return a
-                    
-def FermaTest(r):            
-    for k in range(1,int(2*math.sqrt(r)*math.log2(n))) : #целое число
+def FermaTest(n,r):            
+    for k in range(2,int(2*math.sqrt(r)*math.log2(n))) : #целое число
                 
         if (pow(k,(n-1)) - 1) % n != 0 :
 
@@ -59,29 +72,8 @@ def FermaTest(r):
             continue
         else :
             Output('\nPrime')
-            #exit(0)                            
-    
-f = open(fil,'r')
-n = int(f.read()) #переменная входного числа
+                                    
 
-
-if n < 1 : 
     
-    Output('\nFalse, input is incorrect')
-elif n == 1 :
-    Output('\nFalse, Composite number')
-    
-
-else :
-    PerfectPower()
-    Individual_r()
-         
-        
-    gcd = Individual_r() #переменная вывода НОД + r для Ферма
-            
-    if gcd[0] != 1 :
-        Output('\nFalse, Composite number')
-        #exit(0)
-    FermaTest(gcd[1]) 
        
-            
+Main()            
