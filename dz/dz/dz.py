e4f3dca864cc3e6f3a892a1b47f27b8c2c6298b2
Lincoln123 <zek_95_95@mail.ru>
2015-12-14 03:35:46 +0300

    everything is good
    best code you've ever seen

--- dz/dz/dz.py
+++ dz/dz/dz.py
@@ -1,38 +1,44 @@
 ﻿import math
 import sys
-fil = sys.argv[1]
-fil1 = sys.argv[2] 
 
 def Main():  
-    f = open(fil,'r')
+    f = open(fil, 'r')
     n = int(f.read()) #переменная входного числа
     q = int(4 * pow(math.log2(n),2) + 1)    
+    
     check_input(n)
     PerfectPower(n)
     Individual_r(n,q)
 
 def check_input(n):
+    if len(sys.argv) != 3:
+        Output('\nWrong number of arguments')
+    
+    fil = sys.argv[1]
+    
     if n < 1 : 
         Output('\nFalse, input is incorrect')
+   
     elif n == 1 :
         Output('\nFalse, Composite number')
 
 def Output(text) :
+    fil1 = sys.argv[2]
     f = open(fil1, 'w')
     f.writelines(text)
     f.close()
     exit(0)
      
 def PerfectPower(n):
-    b = 2                        #показатель степени
-    if b > math.log2(n):         #проверка на вход 
-        Output('prime')
+    b = 2
+    if b > math.log2(n):         
+        Output('\nPrime')
         
     while b <= math.log2(n): 
         m = math.ceil(n**(1/b))  #корень b'ой степени из n
         if n == m**b:
             Output('\nFalse, Composite number,Perfect power')
-            #exit(0)
+            
         else:
             b+=1
             
@@ -43,9 +49,10 @@ def Individual_r(n,q):
             GCD(n,r)
             FermaTest(n,r)
             continue
+
         else: 
             q += 1
-            return Individual_r(q)#,[GCD(r),r]
+            return Individual_r(q)
                 
 def GCD(n,r):    
     if r >= n:
@@ -55,6 +62,7 @@ def GCD(n,r):
             if z % gcd == 0 :
                 if gcd != 1:
                     Output('\nFalse, Composite number')        
+    
     else:
         for gcd in range(r,0,-1) :
             z = n
@@ -63,17 +71,14 @@ def GCD(n,r):
                     Output('\nFalse, Composite number')
                     
 def FermaTest(n,r):            
-    for k in range(2,int(2*math.sqrt(r)*math.log2(n))) : #целое число
+    for k in range(2 , int(2 * math.sqrt(r) * math.log2(n))) : #целое число
                 
-        if (pow(k,(n-1)) - 1) % n != 0 :
-
+        if (pow(k , (n - 1)) - 1) % n != 0 :
             Output('\nFalse, Composite number')
-            #exit(0) 
+           
             continue
+
         else :
             Output('\nPrime')
                                     
-
-    
-       
 Main()            
